e09214d0647d753395dc951952c1f6092aacbcf2
mohammedboreny <mohammed.boreny@gmail.com>
2023-02-28 19:13:51 +0300

    Express with hbs files created

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,36 +1,53 @@
-const express=require('express')
+// to create server side
+const express = require('express')
+// to access assets
+const path = require('node:path');
 
-const app=express();
+const hbs=require('hbs')
 
+const partialPath=path.join(__dirname,'../templates/partials')
 
-app.get('',(req,res)=>{
-    res.send('<h1>Hello express!</h1>')
+const app = express();
+
+// setup handlebars hbs  engine and views location and partials
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname,'../templates/views'))
+hbs.registerPartials(partialPath);
+
+// setup static directory to serve
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Weather app",
+        name:"mohammed"
+    })
 })
 
-app.get('/help',(req,res)=>{
-    res.send([{
-        name:'mohammed',
-        age:26
-    }])
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "about me",
+        age:27
+    })
+})
+app.get('/help', (req, res) => 
+   res.render('help', {message:'How can i help you !',title:'help page'})
+)
+
+
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: "",
+        location: {
+            lat: 0,
+            lng: 0
+        }
+    })
 })
 
 
-app.get('/about',(req,res)=>{
-res.send('<h1> title </h1>')
-})
 
-app.get('/weather',(req,res)=>{
-res.send({
-    forecast:"",
-    location:{
-        lat:0,
-        lng:0
-    }
-})
-})
-
-
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
